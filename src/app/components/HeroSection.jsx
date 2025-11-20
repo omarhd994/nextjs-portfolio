435b0298c426ff71f0217d3a4e5fe434d4e39709
omarhd994 <wildsniper94@gmail.com>
2025-11-20 12:03:03 +0100

    Update for PM role and proyects

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -24,9 +24,9 @@ const HeroSection = () => {
               sequence={[
                 "Omar Haddia",
                 1000,
-                "Data Engineer",
+                "Product Manager",
                 1000,
-                "Software Engineer",
+                "Product Owner",
                 1000,
               ]}
               wrapper="span"
@@ -35,7 +35,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            I’m a multilingual tech enthusiast with hands-on experience building end-to-end data pipelines and software solutions, from design to deployment.
+            I’m a multilingual tech enthusiast with hands-on experience building AI-integrated Web and Mobile apps, from market research and design to monetization and launch.
           </p>
           <div>
             <Link
